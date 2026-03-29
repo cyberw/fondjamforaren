aa143ccafbdc0dbdae515729b9caaa028dd195a3
Lars Holmberg <lars.holmberg@redshirt.se>
2026-03-29 16:44:37 +0200

    Make it actually compare stuff

--- analyze.py
+++ analyze.py
@@ -1,25 +1,42 @@
 import glob
+from dataclasses import dataclass
 
 from lxml import etree
 
+
+@dataclass
+class Instrument:
+    isin: str
+    instrumentnamn: str
+    andel: float
+
+
+@dataclass
+class Fund:
+    name: str
+    instruments: list[Instrument]
+
+
 xml_files = glob.glob("fi_data/**/*.xml")
 
-for xml_file in xml_files:
-    with open(xml_file, encoding="utf-8") as file:
-        xml_data = file.read()
 
-    root = etree.fromstring(xml_data)
-    ns = {"fi": "http://schemas.fi.se/publika/vardepappersfonder/20200331"}
+def get_fund(name: str) -> Fund:
+    for xml_file in xml_files:
+        with open(xml_file, encoding="utf-8") as file:
+            xml_data = file.read()
 
-    fondinformation = root.xpath("//fi:Fondinformation/fi:Fond_namn/text()", namespaces=ns)
+        root = etree.fromstring(xml_data)
+        ns = {"fi": "http://schemas.fi.se/publika/vardepappersfonder/20200331"}
 
-    print(fondinformation[0])
+        fondnamn = root.xpath("//fi:Fondinformation/fi:Fond_namn/text()", namespaces=ns)[0]
+        if fondnamn == name:
+            instrument_nodes = root.xpath("//fi:FinansiellaInstrument/fi:FinansielltInstrument", namespaces=ns)
+            instruments = []
+            for instrument in instrument_nodes:
+                if instrument.xpath("./fi:ISIN-kod_instrument/text()", namespaces=ns):
+                    isin = instrument.xpath("./fi:ISIN-kod_instrument/text()", namespaces=ns)[0]
+                    instrumentnamn = instrument.xpath("./fi:Instrumentnamn/text()", namespaces=ns)[0]
+                    andel = float(instrument.xpath("./fi:Andel_av_fondförmögenhet_instrument/text()", namespaces=ns)[0])
+                    instruments.append(Instrument(isin, instrumentnamn, andel))
 
-    instrument_nodes = root.xpath("//fi:FinansiellaInstrument/fi:FinansielltInstrument", namespaces=ns)
-    for instrument in instrument_nodes:
-        if instrument.xpath("./fi:ISIN-kod_instrument/text()", namespaces=ns):
-            isin = instrument.xpath("./fi:ISIN-kod_instrument/text()", namespaces=ns)[0]
-            instrumentnamn = instrument.xpath("./fi:Instrumentnamn/text()", namespaces=ns)[0]
-            andel = float(instrument.xpath("./fi:Andel_av_fondförmögenhet_instrument/text()", namespaces=ns)[0])
-
-        print(f"{instrumentnamn} ({isin}): {andel}%")
+            return Fund(fondnamn, instruments)
---
+++ main.py
@@ -0,0 +1,47 @@
+import logging
+
+import typer
+
+from analyze import Fund, Instrument, get_fund
+
+app = typer.Typer(add_completion=False)
+logger = logging.getLogger(__name__)
+# avoid annoying "Using selector: KqueueSelector" when running in debug:
+logging.getLogger("asyncio").setLevel(logging.INFO)
+
+
+def delta(f1: Fund, f2: Fund) -> list[Instrument]:
+    instrdict: dict[str, Instrument] = {}
+    for instrument in f1.instruments:
+        instrdict[instrument.isin] = instrument
+
+    for instrument in f2.instruments:
+        if instrument.isin in instrdict:
+            instrdict[instrument.isin].andel -= instrument.andel
+        else:
+            instrdict[instrument.isin] = instrument
+            instrdict[instrument.isin].andel = -instrument.andel
+
+    return list(instrdict.values())
+
+
+@app.command()
+def main(
+    fund1: str,
+    fund2: str,
+):
+    """
+    Compare two funds and print the differences.
+    """
+    logger.info(f"Comparing {fund1} and {fund2}...")
+    f1 = get_fund(fund1)
+    f2 = get_fund(fund2)
+    d = delta(f1, f2)
+    d.sort(key=lambda instrument: abs(instrument.andel), reverse=True)
+    for instrument in d:
+        if abs(instrument.andel) > 0.5:
+            print(f"{instrument.isin} {instrument.instrumentnamn} {instrument.andel:.2f}%")
+
+
+if __name__ == "__main__":
+    main("Avanza Global", "Swedbank Robur Access Global")
